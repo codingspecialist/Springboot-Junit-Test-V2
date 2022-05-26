462d45df9bab31307a1b5a9709c30606d8cfe68a
ssar <ssar@nate.com>
2022-05-26 11:57:10 +0900

    Junit 개발환경 profile 구축

---
+++ src/main/java/site/metacoding/blogv4junit/BlogV4JunitApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.blogv4junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogV4JunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogV4JunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/metacoding/blogv4junit/BlogV4JunitApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.blogv4junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogV4JunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
